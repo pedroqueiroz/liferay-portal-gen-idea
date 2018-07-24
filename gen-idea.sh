10b3d0f631d64e0821fa89541b7f65bb34ed581a
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2018-07-24 19:59:07 -0300

    Change script to ask for liferay folder instead of assuming. Also move python script to liferay folder in case it was not moved manually

--- gen-idea.sh
+++ gen-idea.sh
@@ -1,40 +1,38 @@
 #!/bin/sh
 
 # Check if current directory is the liferay-portal project folder
-PORTAL=$PWD
-PORTAL_DEFAULT=$HOME/projects/liferay-portal
+PORTAL=$HOME/projects/liferay-portal
 
 if [ ! -f $PORTAL/gradlew ]; then
-    echo "$PORTAL is not a valid liferay project folder, falling back to $PORTAL_DEFAULT..."
+	echo "$PORTAL is not a valid liferay project folder..."
 
-    PORTAL=$PORTAL_DEFAULT
-    if [ ! -f $PORTAL/gradlew ]; then
-        echo "$PORTAL is also not a valid liferay project folder"
-        exit 1;
-    fi
+	read -p "Please provide the full path where your liferay project folder is located: " PORTAL
 fi
 
 MODULES=(
-    "core/petra/petra-concurrent"
-    "core/petra/petra-executor"
-    "core/petra/petra-function"
-    "core/petra/petra-io"
-    "core/petra/petra-lang"
-    "core/petra/petra-memory"
-    "core/petra/petra-nio"
-    "core/petra/petra-process"
-    "core/petra/petra-reflect"
-    "core/petra/petra-string"
-    "apps/foundation/frontend-taglib/frontend-taglib-clay"
-    "apps/dynamic-data-mapping"
-    "apps/dynamic-data-lists"
+	"core/petra/petra-concurrent"
+	"core/petra/petra-executor"
+	"core/petra/petra-function"
+	"core/petra/petra-io"
+	"core/petra/petra-lang"
+	"core/petra/petra-memory"
+	"core/petra/petra-nio"
+	"core/petra/petra-process"
+	"core/petra/petra-reflect"
+	"core/petra/petra-string"
+	"apps/foundation/frontend-taglib/frontend-taglib-clay"
+	"apps/dynamic-data-mapping"
+	"apps/dynamic-data-lists"
 )
 
 for m in ${MODULES[@]}; do
-  cd $PORTAL/modules/$m
-  $PORTAL/gradlew idea
+	cd $PORTAL/modules/$m
+	$PORTAL/gradlew idea
 done
 
 # Run ./ideactl.py --dwim
-cd $PORTAL
-$(dirname "$0")/ideactl.py --dwim
+if [ ! -f $PORTAL/ideactl.py ]; then
+	cp ./ideactl.py $PORTAL
+fi
+
+$PORTAL/ideactl.py --dwim
